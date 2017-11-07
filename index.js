ed5eafba007c1163597d7df01d6961f45827c180
Jollen Chen <jollen@moko365.com>
2017-11-08 00:16:40 +0800

    create express application

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-'use strict';
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var busboy = require('express-busboy');
 
-module.exports = function() {
-};
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// file upload
+busboy.extend(app, {
+  upload: true,  
+  path: 'public/uploads',  
+});
+
+// utilities
+require('./utilities')(app);
+
+app.use('/', require('./routes'));
+
+module.exports = app;
